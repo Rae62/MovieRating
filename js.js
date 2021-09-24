81c665d18a902e1c8a6ddb7709baf956a906af70
ludovic <ludovic.duhamel-62260@outlook.fr>
2021-09-24 16:06:40 +0200

    java push

--- js.js
+++ js.js
@@ -1,6 +1,4 @@
 
-
-searchBtn.addEventListener("click", expand);
 // carousel début
 (function () {
     var carousel = {
@@ -44,3 +42,133 @@ searchBtn.addEventListener("click", expand);
     carouselControls();
 })();
   // carousel fin
+
+  // affiche scroll
+//  Base de données
+const affiche = [
+  {
+    id: 1,
+    titrefilm: "Flash",
+      annee: '2014',
+      genre: 'science fiction et fantastique',
+      fav1: "img/outline_favorite_white_24dp.png",
+      fav2: 'img/outline_movie_white_24dp.png',
+      fav3: "img/outline_visibility_white_24dp.png",
+      img: 'img/flash-150-x-225.jpg'
+      
+  },
+  {
+      id: 2,
+      titrefilm: "Peaky Blinder",
+      annee: '2013',
+      genre: 'drame',
+      fav1: "img/outline_favorite_white_24dp.png",
+      fav2: 'img/outline_movie_white_24dp.png',
+      fav3: "img/outline_visibility_white_24dp.png",
+      img: 'img/flash-150-x-225.jpg'
+  },
+  {
+    id: 2,
+    titrefilm: "Peaky Blinder",
+    annee: '2013',
+    genre: 'drame',
+    fav1: "img/outline_favorite_white_24dp.png",
+    fav2: 'img/outline_movie_white_24dp.png',
+    fav3: "img/outline_visibility_white_24dp.png",
+    img: 'img/flash-150-x-225.jpg'
+},
+  {
+    id: 2,
+    titrefilm: "Peaky Blinder",
+    annee: '2013',
+    genre: 'drame',
+    fav1: "img/outline_favorite_white_24dp.png",
+    fav2: 'img/outline_movie_white_24dp.png',
+    fav3: "img/outline_visibility_white_24dp.png",
+    img: 'img/flash-150-x-225.jpg'
+},
+  {
+    id: 2,
+    titrefilm: "Peaky Blinder",
+    annee: '2013',
+    genre: 'drame',
+    fav1: "img/outline_favorite_white_24dp.png",
+    fav2: 'img/outline_movie_white_24dp.png',
+    fav3: "img/outline_visibility_white_24dp.png",
+    img: 'img/flash-150-x-225.jpg'
+},
+  {
+    id: 2,
+    titrefilm: "Peaky Blinder",
+    annee: '2013',
+    genre: 'drame',
+    fav1: "img/outline_favorite_white_24dp.png",
+    fav2: 'img/outline_movie_white_24dp.png',
+    fav3: "img/outline_visibility_white_24dp.png",
+    img: 'img/flash-150-x-225.jpg'
+},
+]
+// fin base de données
+const displayFilm = () => {
+  const filmNode = affiche.map( (affiche) => {
+      return createDivElement(affiche);
+  })
+  divAfficheConteneur.append(...filmNode); 
+}
+
+
+
+
+
+
+  // fonction création
+const divAfficheConteneur = document.querySelector('.conteneur');
+
+
+
+const createDivElement = (affiche) => {
+  const divContent = document.createElement('div');
+  divContent.classList.add('content')
+
+  const image = document.createElement('img');
+  image.src = affiche.img
+  image.alt = "Affiche du film Flash"
+
+  const titre = document.createElement('h3')
+  titre.innerText = affiche.titrefilm
+
+  const infoFilm = document.createElement('p')
+  infoFilm.innerText = affiche.annee, affiche.genre
+
+  const a = document.createElement('a');
+  a.href = "#";
+  const logoHeart = document.createElement('img');
+  logoHeart.src = affiche.fav1;
+  logoHeart.alt = "favori";
+  a.innerText = "Add to favorite";
+
+  const a2 = document.createElement('a');
+  a2.href = "#";
+  const logoHeart2 = document.createElement('img');
+  logoHeart2.src =affiche.fav2;
+  logoHeart2.alt = "favori";
+  a2.innerText = "Add to favorite";
+
+  const a3 = document.createElement('a');
+  a3.href = "#";
+  const logoHeart3 = document.createElement('img');
+  logoHeart3.src =affiche.fav3;
+  logoHeart3.alt = "favori";
+  a3.innerText = "Add to favorite";
+
+  divContent.append(image, titre, infoFilm, a, a2, a3);
+  a.appendChild(logoHeart)
+  a2.appendChild(logoHeart2)
+  a3.appendChild(logoHeart3)
+
+return divContent
+
+}
+// fin affiche scroll
+
+displayFilm();
